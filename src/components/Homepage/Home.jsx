215321e1edd7c81e890d402dc7e23d36ae395515
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2022-03-12 11:18:44 +0530

    first_commit_basic

---
+++ src/components/Homepage/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ".home.css";
+import { Navbar } from "../navbar/Navbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      Home
+    </div>
+  );
+};
